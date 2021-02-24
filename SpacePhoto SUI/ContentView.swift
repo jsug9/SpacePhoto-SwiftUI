064af98da2987f1ac2ff9fc46e6c83614c26150d
Augusto Galindo <jesus_augga_99@icloud.com>
2021-02-23 20:57:25 -0500

    Initial Commit

---
+++ SpacePhoto SUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SpacePhoto SUI
+//
+//  Created by Augusto Galindo Alí on 23/02/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SpacePhoto SUI/SpacePhoto_SUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpacePhoto_SUIApp.swift
+//  SpacePhoto SUI
+//
+//  Created by Augusto Galindo Alí on 23/02/21.
+//
+
+import SwiftUI
+
+@main
+struct SpacePhoto_SUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
